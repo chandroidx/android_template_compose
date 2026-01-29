21824d85fa4aefe027ce18ca765fe6ff0131e45e
yc.park <yc.park@deepfine.com>
2026-01-29 10:07:57 +0900

    feature: Modularize features and add ML Kit showcase

--- presentation/home/impl/src/main/kotlin/com/chandroidx/home/model/HomeIntent.kt
+++
@@ -1,9 +0,0 @@
-package com.chandroidx.home.model
-
-sealed interface HomeIntent {
-  data object NavigateToSecondScreen : HomeIntent
-
-  data object NavigateToDialog : HomeIntent
-
-  data object NavigateToBottomSheet : HomeIntent
-}
--- presentation/home/impl/src/main/kotlin/com/chandroidx/home/model/HomeSideEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.chandroidx.home.model
-
-interface HomeSideEffect {
-  data object NavigateToSecondScreen : HomeSideEffect
-
-  data object NavigateToDialog : HomeSideEffect
-
-  data object NavigateToBottomSheet : HomeSideEffect
-}
